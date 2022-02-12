d292324c6c31060d50315d3387ed9719dc742e84
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2022-02-12 03:47:43 +0300

    1.0: fixed github updates

--- services/updates/githubUpdates/src/main/java/com/shatteredpixel/shatteredpixeldungeon/services/updates/GitHubUpdates.java
+++ services/updates/githubUpdates/src/main/java/com/shatteredpixel/shatteredpixeldungeon/services/updates/GitHubUpdates.java
@@ -28,10 +28,9 @@ import com.watabou.noosa.Game;
 import com.watabou.utils.Bundle;
 import com.watabou.utils.DeviceCompat;
 
+import javax.net.ssl.SSLProtocolException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import javax.net.ssl.SSLProtocolException;
 
 public class GitHubUpdates extends UpdateService {
 
@@ -57,7 +56,7 @@ public class GitHubUpdates extends UpdateService {
 		}
 
 		Net.HttpRequest httpGet = new Net.HttpRequest(Net.HttpMethods.GET);
-		httpGet.setUrl("https://api.github.com/repos/00-Evan/shattered-pixel-dungeon/releases");
+		httpGet.setUrl("https://api.github.com/repos/TrashboxBobylev/shutter-pixel-dungeon/releases");
 		httpGet.setHeader("Accept", "application/vnd.github.v3+json");
 
 		Gdx.net.sendHttpRequest(httpGet, new Net.HttpResponseListener() {
